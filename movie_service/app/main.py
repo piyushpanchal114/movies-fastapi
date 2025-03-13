41a09df486c09480a9dd3cc4595ba84f0221ddeb
Piyush Panchal <piyushpanchal114@gmail.com>
2025-03-13 16:15:22 +0530

    removed unused items

--- movie_service/app/main.py
+++ movie_service/app/main.py
@@ -1,32 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-fake_movie_db = [
-    {
-        'name': 'Star Wars: Episode IX - The Rise of Skywalker',
-        'plot': 'The surviving members of the resistance face the First Order',
-        'genres': ['Action', 'Adventure', 'Fantasy'],
-        'casts': ['Daisy Ridley', 'Adam Driver']
-    }
-]
-
-
-class Movie(BaseModel):
-    name: str
-    plot: str
-    genres: list[str]
-    casts: list[str]
-
-
-@app.get("/")
+@app.get("/ping")
 async def ping():
     return {"message": "pong"}
-
-
-@app.get("/movies")
-async def movies() -> list[Movie]:
-    return fake_movie_db
