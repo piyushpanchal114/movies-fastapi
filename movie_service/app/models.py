bbb9d6c3e8f15712ad683ffa2f9a3542dd1050dd
Piyush Panchal <piyushpanchal114@gmail.com>
2025-03-14 23:04:16 +0530

    movie model

--- movie_service/app/models.py
+++ movie_service/app/models.py
@@ -1,9 +1,20 @@
-from pydantic import BaseModel
+import uuid
+from datetime import datetime
+from sqlalchemy import orm, String, JSON, func
 
 
-class Movie(BaseModel):
-    """Model for Movie"""
-    name: str
-    plot: str
-    genres: list[str]
-    casts: list[str]
+class Base(orm.DeclarativeBase):
+    """Base class for declaring model"""
+    pk: orm.Mapped[uuid.UUID] = orm.mapped_column(
+        primary_key=True, default=uuid.uuid4)
+
+
+class Movie(Base):
+    """Movie Schema"""
+    __tablename__ = "movie"
+
+    name: orm.Mapped[str] = orm.mapped_column(String(255), nullable=False)
+    plot: orm.Mapped[str] = orm.mapped_column(String(1000), nullable=True)
+    genres: orm.Mapped[dict] = orm.mapped_column(JSON, default=list)
+    casts: orm.Mapped[dict] = orm.mapped_column(JSON, default=list)
+    created_at: orm.Mapped[datetime] = orm.mapped_column(default=func.now())
